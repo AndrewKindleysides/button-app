4c83c3c2b3d62748d257af0eed66383c8a888218
Andrew Kindleysides <andy.kindleysides@laterooms.com>
2017-07-08 19:03:46 +0100

    clicking button makes post

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
 const PanicButton = require('./');
 const panicButton = new PanicButton();
+var request = require('request');
 
 panicButton.on('pressed', () => {
-  console.log('button pressed!');
+  request.post({
+        url: 'http://localhost:3000/play/everybody'
+    });
 });
