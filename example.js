6d78dfa4ec8f6fc0f58e8340b3830f677c0e7b6d
Andrew Kindleysides <andy.kindleysides@laterooms.com>
2017-07-08 19:57:29 +0100

    updated endpoint for testing

--- example.js
+++ example.js
@@ -4,6 +4,6 @@ var request = require('request');
 
 panicButton.on('pressed', () => {
   request.post({
-        url: 'http://localhost:3000/play/everybody'
+        url: 'http://192.168.0.33:3000/play/everybody'
     });
 });
